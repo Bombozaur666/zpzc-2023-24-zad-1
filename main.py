e329ac86dc79a57deabf578529033c3429dc5efe
Bombo <bombozaur6@gmail.com>
2023-11-30 15:35:56 +0100

    add flask

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import datetime
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_items():
+    html = """
+    <html>
+    <head>
+        <title>Rozwiązanie zadania PZC 2.1</title>
+    </head>
+    <body>
+    """
+    html += datetime.datetime.now()
+    html +=  """
+    </body>
+    """
+
+    return html
